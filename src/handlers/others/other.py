f4903066971c220770d42d669ebeae58e133a9ee
Windows 10 <adhambekhusinboyev@gmail.com>
2025-07-16 15:10:37 +0500

    first

--- src/handlers/others/other.py
+++ src/handlers/others/other.py
@@ -8,12 +8,12 @@ from src.keyboards.buttons import UserPanels
 other_router = Router()
 # some code...
 
-@other_router.message()
-async def chosen_lang(message: Message, state: FSMContext):
-    try:
-        await message.delete()
-        await state.clear()
-    except: pass
-    await message.answer(
-        "<b>Botimizdan foydalanishni davom etishingiz mumkin, quyidagi tugmalardan birini tanlab davom etishingiz mumkin</b>",
-        reply_markup=await UserPanels.main2(), parse_mode="html")
+# @other_router.message()
+# async def chosen_lang(message: Message, state: FSMContext):
+#     try:
+#         await message.delete()
+#         await state.clear()
+#     except: pass
+#     await message.answer(
+#         "<b>Botimizdan foydalanishni davom etishingiz mumkin, quyidagi tugmalardan birini tanlab davom etishingiz mumkin</b>",
+#         reply_markup=await UserPanels.main2(), parse_mode="html")
