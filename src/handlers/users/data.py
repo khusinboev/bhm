22eac9ae9fc3900f88205857843a35ad9eab0a3c
Windows 10 <adhambekhusinboyev@gmail.com>
2025-07-16 09:01:46 +0500

    first

--- src/handlers/users/data.py
+++ src/handlers/users/data.py
@@ -15,13 +15,16 @@ data_router = Router()
 
 def get_abiturient_info_by_id(user_id: str) -> str:
     options = Options()
-    # options.add_argument("--headless=new")  # "new" rejim Chrome 109+ uchun kerakli
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument(
+        "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+    )
 
     driver = webdriver.Chrome(options=options)
 
