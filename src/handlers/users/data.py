787815ee147f3c5e27df0c12576b3fda62137dac
Windows 10 <adhambekhusinboyev@gmail.com>
2025-07-23 21:37:31 +0500

    first

--- src/handlers/users/data.py
+++ src/handlers/users/data.py
@@ -221,4 +221,4 @@ async def handle_id_query(msg: Message):
     except Exception as e:
         logging.exception("❌ Ichki xatolik:")
         await loading_msg.delete()
-        await msg.answer("❌ Ichki xatolik yuz berdi. Iltimos, keyinroq urinib ko'ring.")
+        await msg.answer("🚨 Hozirda juda ko‘p so‘rovlar bo‘layapti.\nIltimos, 30 soniyadan keyin qayta urinib ko‘ring.")
